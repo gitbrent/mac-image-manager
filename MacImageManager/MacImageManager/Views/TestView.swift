5423a55a35bc8237b8497514c6b56dba372d34d8
Brent Ely <gitbrent@users.noreply.github.com>
2025-09-27 12:47:16 -0500

    added lightweight test view

---
+++ MacImageManager/MacImageManager/Views/TestView.swift
@@ -0,0 +1,19 @@
+//
+//  TestView.swift
+//  MacImageManager
+//
+//  Created by Brent Ely on 9/23/25.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .padding()
+    }
+}
+
+#Preview {
+    TestView()
+}
